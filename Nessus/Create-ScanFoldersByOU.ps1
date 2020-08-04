5fe3375c11ef0f2144044cf05b63e3296c1ff2bc
nathan mcnulty <mcnultyna@beaverton.k12.or.us>
2020-08-03 17:12:06 -0700

    Create Nessus scan folders

---
+++ Nessus/Create-ScanFoldersByOU.ps1
@@ -0,0 +1,29 @@
+# Set variables
+$ou = "OU=T1,OU=Servers,DC=domain,DC=com"
+$uri = "https://nessus.domain.com:8834/folders"
+
+# Set up authentication
+if (Test-Path -Path $env:USERPROFILE\nessus.xml) {
+    $key = Import-Clixml -Path $env:USERPROFILE\nessus.xml -Force
+} else {
+    $key = @{
+        accessKey = Read-Host "Enter access key"
+        secretKey = Read-Host "Enter secret key"
+    }
+    $save = Read-Host "Would you like to save this for future use? (Y/N)"
+    if ($save -eq "Y") { Export-Clixml -InputObject $key -Path $env:USERPROFILE\nessus.xml -Force }
+}
+
+# Set up headers for authentication
+$headers=@{}
+$headers.Add("X-ApiKeys", "accessKey=$($key.accessKey);secretKey=$($key.secretKey)")
+
+
+
+# Get list of existing folders in Nessus
+$existing = (Invoke-RestMethod -Uri $uri -Method Get -Headers $headers).folders.name
+
+# Create base folders if they don't exist
+(Get-ADObject -Filter { objectClass -eq "organizationalUnit" } -SearchBase $ou -SearchScope OneLevel).name | ForEach-Object { 
+    if ( $_ -notin $existing ) { Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body @{name="$_"} }
+}
