0a7414dd3fb056460cf5e5d0661631cb4518685a
nathan mcnulty <mcnultyna@beaverton.k12.or.us>
2020-10-08 13:21:48 -0700

    Nested group support, added output per request

--- Azure/MyStaff/Update-MyStaffAUMembers.ps1
+++ Azure/MyStaff/Update-MyStaffAUMembers.ps1
@@ -20,21 +20,28 @@ $excludedGroups = "IT-Staff","AdminGroups"
 (Get-AzureADGroup -SearchString "$prefix") | ForEach-Object {
     $AU = ($_.DisplayName).Replace("$prefix-","")
     $AUobjId = (Get-AzureADAdministrativeUnit -Filter "displayname eq '$AU'").objectId
-    $existingUsers = (Get-AzureADAdministrativeUnitMember -ObjectId $AUobjID -All $true).objectId
-    $currentUsers = (Get-AzureADGroup -SearchString $AU | Get-AzureADGroupMember -All $true).objectId
-    $excludedUsers = ($excludedGroups | ForEach-Object { Get-AzureADGroup -SearchString $_ | Get-AzureADGroupMember -All $true }).objectId
+    $existingUsers = Get-AzureADAdministrativeUnitMember -ObjectId $AUobjID -All $true
+    $currentUsers = Get-AzureADGroup -SearchString $AU | Get-AzureADGroupMember -All $true | ForEach-Object { 
+        # Add support for single depth nested groups; add more if you need to
+        if ($_.objectType -eq "Group") { $_ | Get-AzureADGroupMember -All $true } else { $_ }
+    }
+    $excludedUsers = $excludedGroups | ForEach-Object { Get-AzureADGroup -SearchString $_ | Get-AzureADGroupMember -All $true }
 
     # Add members to AU
     $currentUsers | ForEach-Object {
-        if ($_ -notin $existingUsers -and $_ -notin $excludedUsers) {
-            Add-AzureADAdministrativeUnitMember -ObjectId $AUobjID -RefObjectId $_
+        if ($_.objectId -notin $existingUsers.objectId -and $_.objectId -notin $excludedUsers.objectId) {
+            $userObjId = $_.objectId
+            Add-AzureADAdministrativeUnitMember -ObjectId $AUobjID -RefObjectId $userObjId
+            Write-Output "Added $($_.displayName) to $AU"
         }
     }
 
     # Remove members from AU
     $existingUsers | ForEach-Object {
-        if ($_ -notin $currentUsers) {
-            Remove-AzureADAdministrativeUnitMember -ObjectId $AUobjID -MemberId $_
+        if ($_.objectId -notin $currentUsers.objectId) {
+            $userObjId = $_.objectId
+            Remove-AzureADAdministrativeUnitMember -ObjectId $AUobjID -MemberId $userObjId
+            Write-Output "Removed $($_.displayName) from $AU"
         }
     }
 }
