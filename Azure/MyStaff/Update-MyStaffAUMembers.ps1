37460600122f5212394ab7e6fac87cc43011a5a1
nathan mcnulty <mcnultyna@beaverton.k12.or.us>
2020-10-09 15:03:28 -0700

    Lots of updates

--- Azure/MyStaff/Update-MyStaffAUMembers.ps1
+++ Azure/MyStaff/Update-MyStaffAUMembers.ps1
@@ -8,37 +8,60 @@
     Please feel free to ask me questions on Twitter: @nathanmcnulty
 #>
 
-Connect-AzureAD
+# $creds = Import-CliXml -Path <scrit path>\creds.xml
+Connect-AzureAD #-Credential $creds
 
 # This prefix matches the prefix you used in the prereqs script
 $prefix = "AUPWAdmins"
 
 # Restrict users from being added to AU's (prevent privilege escalation)
 $excludedGroups = "IT-Staff","AdminGroups"
+$excludedUsers = $excludedGroups | ForEach-Object { Get-AzureADGroup -SearchString $_ | Get-AzureADGroupMember -All $true }
+
+function AddUserToAU {
+    [CmdletBinding()]
+    param ([Parameter(ValueFromPipeline=$true)]
+        $userObj
+    )
+
+    if ($userObj.objectId -notin $excludedUsers.objectId) {
+        $userObjId = $userObj.objectId
+        Add-AzureADAdministrativeUnitMember -ObjectId $AUobjId -RefObjectId $userObjId
+        Write-Output "Added $($userObj.displayName) to $AU"
+    }
+}
+
+function RemoveUserFromAU {
+    [CmdletBinding()]
+    param ([Parameter(ValueFromPipeline=$true)]
+        $userObj
+    )
+
+    $userObjId = $userObj.objectId
+    $userDisplayname = (Get-AzureADUser -ObjectId $userObjId).displayName
+    Remove-AzureADAdministrativeUnitMember -ObjectId $AUobjId -MemberId $userObjId
+    Write-Output "Removed $userDisplayName from $AU"
+}
 
 # Grab list of AUPWAdmin groups and evaluate membership of those against the roles on the AU's
 (Get-AzureADGroup -SearchString "$prefix") | ForEach-Object {
     $AU = ($_.DisplayName).Replace("$prefix-","")
     $AUobjId = (Get-AzureADAdministrativeUnit -Filter "displayname eq '$AU'").objectId
-    $existingUsers = Get-AzureADAdministrativeUnitMember -ObjectId $AUobjID -All $true
+    $existingUsers = Get-AzureADAdministrativeUnitMember -ObjectId $AUobjId -All $true
     $currentUsers = Get-AzureADGroup -SearchString $AU | Get-AzureADGroupMember -All $true | ForEach-Object { 
         # Add support for single depth nested groups; add more if you need to
         if ($_.objectType -eq "Group") { $_ | Get-AzureADGroupMember -All $true } else { $_ }
     }
-    $excludedUsers = $excludedGroups | ForEach-Object { Get-AzureADGroup -SearchString $_ | Get-AzureADGroupMember -All $true }
-
-    $list = Compare-Object -ReferenceObject $currentUsers -DifferenceObject $existingUsers -Property objectId -PassThru
-    $list | Where-Object { $_.SideIndicator -eq "<=" } | ForEach-Object {
-        if ($_.objectId -notin $excludedUsers.objectId) {
-            $userObjId = $_.objectId
-            Add-AzureADAdministrativeUnitMember -ObjectId $AUobjID -RefObjectId $userObjId
-            Write-Output "Added $($_.displayName) to $AU"
-        }
-    }
-    $list | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object {
-        $userObjId = $_.objectId
-        $userDisplayname = (Get-AzureADUser -ObjectId $userObjId).displayName
-        Remove-AzureADAdministrativeUnitMember -ObjectId $AUobjID -MemberId $userObjId
-        Write-Output "Removed $userDisplayName from $AU"
+    if ($currentUsers -eq $null -and $existingUsers -eq $null) { 
+        Write-Output "Both groups are empty"
+        break
+    } elseif ($existingUsers -eq $null -and -not $currentUsers -eq $null) {
+        $currentUsers | AddUserToAU $_
+    } elseif ($currentUsers -eq $null -and -not $existingUsers -eq $null) {
+        $currentUsers | RemoveUsersFromAU $_
+    } else {
+        $list = Compare-Object -ReferenceObject $currentUsers -DifferenceObject $existingUsers -Property objectId -PassThru
+        $list | Where-Object { $_.SideIndicator -eq "<=" } | ForEach-Object { AddUserToAU $_ }
+        $list | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object { RemoveUserFromAU $_ }
     }
 }
--- Azure/MyStaff/Update-MyStaffAUPWAdmins.ps1
+++ Azure/MyStaff/Update-MyStaffAUPWAdmins.ps1
@@ -8,7 +8,8 @@
     Please feel free to ask me questions on Twitter: @nathanmcnulty
 #>
 
-Connect-AzureAD
+# $creds = Import-CliXml -Path <scrit path>\creds.xml
+Connect-AzureAD #-Credential $credss
 
 # This prefix matches the prefix you used in the prereqs script
 $prefix = "AUPWAdmins"
@@ -16,29 +17,47 @@ $prefix = "AUPWAdmins"
 # You can change the role if you'd like admins to have ability reset MFA or edit user properties
 $role = (Get-AzureADDirectoryRole | Where-Object -Property DisplayName -EQ -Value "Password Administrator").objectId
 
+function AddAdminToAURole {
+    [CmdletBinding()]
+    param ([Parameter(ValueFromPipeline=$true)]
+        $userObj
+    )
+
+    $roleMember = New-Object -TypeName Microsoft.Open.AzureAD.Model.RoleMemberInfo
+    $roleMember.objectId = $userObj.objectId
+    Add-AzureADScopedRoleMembership -ObjectId $AUobjId -RoleObjectId $role -RoleMemberInfo $roleMember
+}
+
+function RemoveAdminFromAURole {
+    [CmdletBinding()]
+    param ([Parameter(ValueFromPipeline=$true)]
+        $userObj
+    )
+
+    $userObjId = $userObj.objectId
+    Get-AzureAdScopedRoleMembership -ObjectId $AUobjId | Where-Object { $userObj.RoleMemberInfo.ObjectId -eq $userObjId } | ForEach-Object { 
+        Remove-AzureAdScopedRoleMembership -ObjectId $AUobjId -ScopedRoleMembershipId $_.id
+    }
+}
+
 # Grab list of AUPWAdmin groups and evaluate membership of those against the roles on the AU's
 (Get-AzureADGroup -SearchString "$prefix") | ForEach-Object {
     $AU = ($_.DisplayName).Replace("$prefix-","")
     $AUobjId = (Get-AzureADAdministrativeUnit -Filter "displayname eq '$AU'").objectId
-    $existingAdmins = (Get-AzureADScopedRoleMembership -ObjectId $AUobjID).roleMemberInfo.objectId
-    $currentAdmins = (Get-AzureADGroupMember -ObjectID $_.objectId).objectId
+    $existingAdmins = (Get-AzureADScopedRoleMembership -ObjectId $AUobjID).roleMemberInfo
+    $currentAdmins = Get-AzureADGroupMember -ObjectID $_.objectId
 
-    # Add admins
-    $currentAdmins | ForEach-Object { 
-        if ($_ -notin $existingAdmins) {
-            $roleMember = New-Object -TypeName Microsoft.Open.AzureAD.Model.RoleMemberInfo
-            $roleMember.objectId = $_
-            Add-AzureADScopedRoleMembership -ObjectId $AUobjId -RoleObjectId $role -RoleMemberInfo $roleMember
-        }
-    }
-
-    # Remove admins
-    $existingAdmins | ForEach-Object {
-        if ($_ -notin $currentAdmins) {
-            $userObjId = $_
-            Get-AzureAdScopedRoleMembership -ObjectId $AUobjId | Where-Object { $_.RoleMemberInfo.ObjectId -eq $userObjId } | ForEach-Object { 
-                Remove-AzureAdScopedRoleMembership -ObjectId $AUobjId -ScopedRoleMembershipId $_.id
-            }
-        }
+    # Compare-Object won't work on null values, so if all members of a group are removed, this logic makes sure they get removed
+    if ($currentAdmins -eq $null -and $existingAdmins -eq $null) { 
+        Write-Output "Both groups are empty"
+        break
+    } elseif ($existingAdmins -eq $null -and -not $currentAdmins -eq $null) {
+        $currentUsers | AddAdminToAURole $_
+    } elseif ($currentAdmins -eq $null -and -not $existingAdmins -eq $null) {
+        $currentUsers | RemoveAdminFromAURole $_
+    } else {
+        $list = Compare-Object -ReferenceObject $currentAdmins -DifferenceObject $existingAdmins -Property objectId -PassThru
+        $list | Where-Object { $_.SideIndicator -eq "<=" } | ForEach-Object { AddAdminToAURole $_ }
+        $list | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object { RemoveAdminFromAURole $_ }
     }
 }
