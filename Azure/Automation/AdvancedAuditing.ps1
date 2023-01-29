f2dba1ab9d1bd8170888372073df2679bc0e2b36
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2023-01-29 13:58:19 -0800

    Add Advanced Auditing

---
+++ Azure/Automation/AdvancedAuditing.ps1
@@ -0,0 +1,20 @@
+# Connect to Microsoft Graph within Azure Automation
+Connect-AzAccount -Identity
+$token = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
+Connect-MgGraph -AccessToken $token.Token
+
+# Get members of Advanced Auditing dynamic group
+$groupId = (Get-MgGroup -Filter "DisplayName eq 'Azure Automation - Advanced Auditing'").Id
+$groupMembers = (Get-MgGroupMember -GroupId $groupId | Select-Object -ExpandProperty AdditionalProperties).mail
+
+# Connect to Exchange Online using Managed Identity
+Connect-ExchangeOnline -ManagedIdentity -Organization getsecurer.onmicrosoft.com -Verbose
+
+# I have absolutely no idea why the script fails without running a Get-Mailbox first...
+Get-Mailbox -Identity $groupMembers[0] | Out-Null
+
+# Enable all advanced auditing
+$groupMembers | ForEach-Object {
+    Write-Output $_
+    Set-Mailbox -Identity $_ -AuditAdmin @{add="Create","FolderBind","SendAs","SendOnBehalf","SoftDelete","HardDelete","Update","Move","MoveToDeletedItems","UpdateFolderPermissions","ApplyRecord","RecordDelete","Send","UpdateCalendarDelegation","UpdateComplianceTag","UpdateInboxRules","MailItemsAccessed"} -AuditDelegate @{add="Create","FolderBind","SendAs","SendOnBehalf","SoftDelete","HardDelete","Update","Move","MoveToDeletedItems","UpdateFolderPermissions","ApplyRecord","MailItemsAccessed","RecordDelete","UpdateComplianceTag","UpdateInboxRules"} -AuditOwner @{add="Create","SoftDelete","HardDelete","Update","Move","MoveToDeletedItems","UpdateFolderPermissions","ApplyRecord","RecordDelete","Send","UpdateCalendarDelegation","UpdateComplianceTag","UpdateInboxRules","MailItemsAccessed","MailboxLogin","SearchQueryInitiated"}
+}
