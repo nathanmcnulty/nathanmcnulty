4d721b15e2e08dc4d82d18c82efbbcae745cd1ff
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2023-04-22 23:01:33 -0700

    Fixed days filter
    Forgot to change days back after testing

--- Azure/Automation/Device-Cleanup-v2.ps1
+++ Azure/Automation/Device-Cleanup-v2.ps1
@@ -2,7 +2,7 @@
 Connect-MgGraph -Identity
 
 # Get Zulu formatted time from 90 days ago for filter
-$date = (Get-Date (Get-Date).AddDays(-13) -Format u).Replace(' ','T')
+$date = (Get-Date (Get-Date).AddDays(-90) -Format u).Replace(' ','T')
 
 # Get devices that haven't signed in for more than 90 days and disable them
 Get-MgDevice -All -Filter "ApproximateLastSignInDateTime le $date"  | ForEach-Object { 
