0b193a21d71bb1f2018f752e3129664fa25159ee
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-03-06 21:25:35 -0900

    Done with testing

--- Intune/Find-AppsWithAPPApplied.ps1
+++ Intune/Find-AppsWithAPPApplied.ps1
@@ -1,19 +1,21 @@
 # Get all iOS managed app protection policies that have been applied to at least one user and list the apps that are protected by these policies
-$appApps = New-Object System.Collections.ArrayList
+$iOSApps = New-Object System.Collections.ArrayList
 (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/iosManagedAppProtections").value.id | ForEach-Object { 
-    (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/iosManagedAppProtections/$_`?`$expand=apps,assignments,deploymentSummary" -OutputType PSObject).deploymentSummary.configurationDeploymentSummaryPerApp | Where-Object { $_.configurationAppliedUserCount -eq 0 } | ForEach-Object {
-        $appApps.Add($_.mobileAppIdentifier.bundleId)
+    (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/iosManagedAppProtections/$_`?`$expand=apps,assignments,deploymentSummary" -OutputType PSObject).deploymentSummary.configurationDeploymentSummaryPerApp | Where-Object { $_.configurationAppliedUserCount -ne 0 } | ForEach-Object {
+        $iOSApps.Add($_.mobileAppIdentifier.bundleId) | Out-Null
     }
 }
-Write-Output "List of iOS apps where APP has been applied:`n"
-$appApps | Select-Object -Unique
 
 # Get all Android managed app protection policies that have been applied to at least one user and list the apps that are protected by these policies
-$appApps = New-Object System.Collections.ArrayList
+$androidApps = New-Object System.Collections.ArrayList
 (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/AndroidManagedAppProtections").value.id | ForEach-Object { 
-    (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/AndroidManagedAppProtections/$_`?`$expand=apps,assignments,deploymentSummary" -OutputType PSObject).deploymentSummary.configurationDeploymentSummaryPerApp | Where-Object { $_.configurationAppliedUserCount -eq 0 } | ForEach-Object {
-        $appApps.Add($_.mobileAppIdentifier.packageId)
+    (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceAppManagement/AndroidManagedAppProtections/$_`?`$expand=apps,assignments,deploymentSummary" -OutputType PSObject).deploymentSummary.configurationDeploymentSummaryPerApp | Where-Object { $_.configurationAppliedUserCount -ne 0 } | ForEach-Object {
+        $androidApps.Add($_.mobileAppIdentifier.packageId) | Out-Null
     }
 }
+
+Write-Output "List of iOS apps where APP has been applied:`n"
+$iOSApps | Select-Object -Unique
+
 Write-Output "`n`nList of Android apps where APP has been applied:`n"
-$appApps | Select-Object -Unique
+$androidApps | Select-Object -Unique
