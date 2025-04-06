0a26d0f5ed9bb8c0a053b265ae2c7299390fda5b
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-04-05 16:11:27 -0800

    fix-sp-less-apps

---
+++ Entra/fix-sp-less-apps.ps1
@@ -0,0 +1,4 @@
+Get-MgBetaAuditLogSignIn -Filter "signInEventTypes/any(t: t eq 'servicePrincipal') and servicePrincipalId eq '00000000-0000-0000-0000-000000000000'" | ForEach-Object {
+    New-MgBetaServicePrincipal -AppId $_.appId
+    Add-Content -Path .\changes.txt Value "Registered $($_.appId). To undo, run Remove-MgBetaServicePrincipal -AppId $($_.appId)"
+}
