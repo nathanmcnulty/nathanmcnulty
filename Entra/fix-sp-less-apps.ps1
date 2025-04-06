de867e82b85283e16d28ab28cdf687a47770fc27
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-04-05 16:16:21 -0800

    change to using out-gridview

--- Entra/fix-sp-less-apps.ps1
+++ Entra/fix-sp-less-apps.ps1
@@ -1,4 +1,4 @@
-Get-MgBetaAuditLogSignIn -Filter "signInEventTypes/any(t: t eq 'servicePrincipal') and servicePrincipalId eq '00000000-0000-0000-0000-000000000000'" | ForEach-Object {
+Get-MgBetaAuditLogSignIn -Filter "signInEventTypes/any(t: t eq 'servicePrincipal') and servicePrincipalId eq '00000000-0000-0000-0000-000000000000'" | Out-GridView -PassThru | ForEach-Object {
     New-MgBetaServicePrincipal -AppId $_.appId
     Add-Content -Path .\changes.txt Value "Registered $($_.appId). To undo, run Remove-MgBetaServicePrincipal -AppId $($_.appId)"
 }
