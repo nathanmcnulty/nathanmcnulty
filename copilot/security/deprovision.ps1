542a8ab727b1c5eeefb7b96e5613c97f38725a68
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-12-28 20:34:36 -0900

    Update deprovision.ps1

--- copilot/security/deprovision.ps1
+++ copilot/security/deprovision.ps1
@@ -1,11 +1,11 @@
 # Populate values, may need to define subscription if MI given access to multiple subs
 $name = "security-copilot"
-$subscriptionId = "24231b54-f7ab-486d-8522-936f3dddab17"
+$subscriptionName = "sub-security-copilot"
 
 # Connect to Azure as Managed Identity
 Disable-AzContextAutosave -Scope Process | Out-Null
-Connect-AzAccount #-Identity
-Set-AzContext -SubscriptionName $subscriptionId | Out-Null
+Connect-AzAccount -Identity | Out-Null
+$subscriptionId = (Set-AzContext -Subscription $subscriptionName).Subscription.Id
 
 # Delete resource to deprovision SCU
 Remove-AzResource -ResourceId "/subscriptions/$subscriptionId/resourceGroups/$name/providers/Microsoft.SecurityCopilot/capacities/$name" -Force -ErrorAction SilentlyContinue
